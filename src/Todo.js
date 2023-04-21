2e6dd6008d8401ccdcbae7ff78b9046fc5d375bb
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-21 13:52:29 -0400

    add classnames

--- src/Todo.js
+++ src/Todo.js
@@ -11,8 +11,11 @@ import React from "react";
 function Todo({id, title, description, priority}) {
   return (
       <div className="Todo">
-        <div><b>{title}</b> <small>(priority: {priority})</small></div>
-        <div><small>{description}</small></div>
+        <div>
+          <b className="Todo-title">{title}</b>
+          <small className="Todo-priority">(priority: {priority})</small>
+        </div>
+        <div><small className="Todo-description">{description}</small></div>
       </div>
   );
 }
