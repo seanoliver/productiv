94fb360f70ba9a190c518d10febc5bb85334d2e7
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-21 13:52:38 -0400

    add unit test

---
+++ src/Todo.test.js
@@ -0,0 +1,69 @@
+import { render, fireEvent } from "@testing-library/react";
+import Todo from "./Todo.js";
+
+const TEST_TODO = {
+  id: "im-an-id",
+  title: "Test title",
+  description: "Test description",
+  priority: 1
+};
+
+it("renders without crashing", function () {
+  render(<Todo
+    id={TEST_TODO.id}
+    title={TEST_TODO.title}
+    description={TEST_TODO.description}
+    priority={TEST_TODO.priority}
+  />);
+});
+
+it("matches snapshot", function () {
+  const { container, debug } = render(<Todo
+    id={TEST_TODO.id}
+    title={TEST_TODO.title}
+    description={TEST_TODO.description}
+    priority={TEST_TODO.priority}
+  />);
+  expect(container).toMatchSnapshot();
+});
+
+// make sure title, description, priority are on page
+it("has the correct title", function () {
+  const { container, debug } = render(<Todo
+    id={TEST_TODO.id}
+    title={TEST_TODO.title}
+    description={TEST_TODO.description}
+    priority={TEST_TODO.priority}
+  />);
+
+  const title = container.querySelector(".Todo-title");
+
+  expect(title.textContent).toMatch("Test title");
+});
+
+it("has the correct description", function () {
+  const { container, debug } = render(<Todo
+    id={TEST_TODO.id}
+    title={TEST_TODO.title}
+    description={TEST_TODO.description}
+    priority={TEST_TODO.priority}
+  />);
+
+  const description = container.querySelector(".Todo-description");
+
+  expect(description.textContent).toMatch("Test description");
+});
+
+it("has the correct priority", function () {
+  const { container, debug } = render(<Todo
+    id={TEST_TODO.id}
+    title={TEST_TODO.title}
+    description={TEST_TODO.description}
+    priority={TEST_TODO.priority}
+  />);
+
+  const priority = container.querySelector(".Todo-priority");
+
+  expect(priority.textContent).toMatch("1");
+});
+
